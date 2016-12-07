ff8ddf052c6f28ce8f037ea449fdca1f9de9b230
SandiKoren <sandi.koren1@gmail.com>
2016-12-07 23:31:48 +0100

    text

--- js/animated-text.js
+++ js/animated-text.js
@@ -11,7 +11,7 @@ jQuery(document).ready(function($){
 		selectionDuration = 500,
 		typeAnimationDelay = selectionDuration + 800,
 		//clip effect
-		revealDuration = 600,
+		revealDuration = 1600,
 		revealAnimationDelay = 1500;
 
 	initHeadline();
