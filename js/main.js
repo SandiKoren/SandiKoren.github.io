41cd83fb6d598aaf7349a228f480a95ab42fa859
SandiKoren <sandi.koren1@gmail.com>
2016-12-09 19:26:08 +0100

    final version of site

--- js/main.js
+++ js/main.js
@@ -122,6 +122,25 @@ $("#third-section[href^='#']").on('click', function (e) {
 });
 
 
+$(".first-section .first-text a[href^='#']").on('click', function (e) {
+
+	// prevent default anchor click behavior
+	e.preventDefault();
+
+	// store hash
+	var hash = this.hash;
+
+	// animate
+	$('html, body').animate({
+		scrollTop: $(hash).offset().top - 70
+	}, 600, function () {
+
+		// when done, add hash to url
+		// (default click behaviour)
+		window.location.hash = hash;
+	});
+
+});
 $("#fourth-section[href^='#']").on('click', function (e) {
 
 	// prevent default anchor click behavior
